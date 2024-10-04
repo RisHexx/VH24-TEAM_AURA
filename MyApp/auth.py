229d1da519b00e8c8291fabe470af4b78f5ef059
RisHexx <rishabhkanojiya9082@gmail.com>
2024-10-04 13:11:53 +0530

    new commit

--- MyApp/auth.py
+++ MyApp/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint , render_template
 
 auth = Blueprint("auth",__name__)
 
 @auth.route("/login",methods=['GET','POST'])
 def login():
-    return "Login Page"
+    return render_template("index.html")
 
 @auth.route("/signup",methods=['GET','POST'])
 def signup():
